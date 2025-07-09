09bc70d7d007c7107b9b3b4a5fa0508f2e5dd617
krishna d <navipatil939@gmail.com>
2025-07-09 00:10:30 -0700

    this is branch1

---
+++ branch1.py
@@ -0,0 +1,16 @@
+# lis = [12.5, 45, 'meera', 'priya', "cherry", 23.6]
+# print(lis)
+#
+# lis.insert(2, 'banana')
+# print(lis)
+# del lis[5]
+# print(lis)
+
+
+lis = [23.6, 89, 34, 45, 12, 12]
+
+print(lis)
+lis.insert(1, 'wealth')
+print(lis)
+del lis[4]
+print(lis)
